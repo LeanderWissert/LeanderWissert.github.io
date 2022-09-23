c2164c8302d910bbcd38756d3a8a43e965b15150
Leander Wissert <93265202+LeanderWissert@users.noreply.github.com>
2022-09-23 08:53:37 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-  createCanvas(800, 400);
+let zBereich;
+
+function setup() { //einmal auszuführen
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0); //links oben
+  zBereich.style('z-index','-1'); //Verschiebung auf der z-Achse
 }
 
 function draw() {
-  background(220);
+  // Ohne background = transparent
+  noStroke;
+  fill(255, 50);
+  circle(mouseX, mouseY, 35);
 }
